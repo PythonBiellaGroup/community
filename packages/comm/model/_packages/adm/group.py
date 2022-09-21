c17c2bfe4c12884b2fc26d34258c5e02be27b3ee
Davide Paci <davide.paci@softwell.it>
2022-09-21 18:39:23 +0200

    pbg instance now shares community package with Genropy

--- packages/comm/model/_packages/adm/group.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-from gnr.core.gnrdecorator import metadata
-
-class Table(object):
-
-    @metadata(mandatory=True)
-    def sysRecord_COMM(self):
-        return self.newrecord(code='COMM', description='!![en]Community Developer')
--- packages/pbg/main.py
+++ packages/pbg/main.py
@@ -4,8 +4,8 @@ from gnr.app.gnrdbo import GnrDboTable, GnrDboPackage
 
 class Package(GnrDboPackage):
     def config_attributes(self):
-        return dict(comment='Community package',sqlschema='comm',sqlprefix=True,
-                    name_short='!![en]Community', name_long='!![en]Community', name_full='!![en]Community')
+        return dict(comment='Python Biella Group package',sqlschema='pbg',sqlprefix=True,
+                    name_short='!![en]PBG', name_long='!![en]Python Biella Group', name_full='!![en]Python Biella Community')
                     
     def config_db(self, pkg):
         pass
